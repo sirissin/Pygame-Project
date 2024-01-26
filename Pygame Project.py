f34e2b0da9873a56a9a66cd10e261d447afc3e6c
Siris <aperry2171@gmail.com>
2024-01-25 23:07:32 -0500

    Tested out drawing a circle, rectangle, and triangle. All

--- Pygame Project.py
+++ Pygame Project.py
@@ -21,8 +21,9 @@ purple = pygame.Color(255, 0, 255, 1)
 # Defined colors for specific uses
 background_color = white
 
-# Creating a rectangle and its parameters
+# Creating shapes and their parameters
 rect = pygame.Rect(50, 10, 200, 100)
+triangle_coordinates = [(100, 50), (125, 100), (75, 100)]
 
 # Looking for the user closing the game
 while True:
@@ -34,8 +35,14 @@ while True:
     # Screen color
     screen.fill(background_color)
 
-    # Drawing a rectangle and changing its properties
-    pygame.draw.rect(screen, red, rect, 5, 20)
+    # Drawing a rectangle and changing its properties (coordinates are for top right corner)
+    # pygame.draw.rect(screen, red, rect, 5, 20)
+
+    # Drawing a circle (coordinates are for center)
+    # pygame.draw.circle(screen, "blue", (100, 100), 50)
+
+    # Drawing a polygon, specifically a triangle
+    # pygame.draw.polygon(screen, "green", triangle_coordinates)
 
     # Refreshing screen
     pygame.display.flip()
