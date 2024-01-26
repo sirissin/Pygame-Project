0ff6cb45d1610e0f388c8d440f3d2d2d922577af
Siris <aperry2171@gmail.com>
2024-01-25 22:25:47 -0500

    Set window size and name.
    Set frame rate and background color.

--- Pygame Project.py
+++ Pygame Project.py
@@ -5,9 +5,20 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
+# Window settings and name
+screen = pygame.display.set_mode((600, 500))
+pygame.display.set_caption("My Game")
+
+# Looking for the user closing the game
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    # Screen updating and background color
+    screen.fill("white")
+    pygame.display.flip()
+
+    # Setting the frame rate
     clock.tick(60)
